ce229aba641b7f37b08e6a0f57e147a5d30fed1a
Verumun <nevezekiel3@gmail.com>
2023-06-02 18:53:14 +0100

    modification

--- javascript/script.js
+++ javascript/script.js
@@ -3,20 +3,32 @@
 const diceBtnEl = document.getElementById('button');
 const adviceTextEl = document.getElementById('advice-text');
 const adviceHeaderEl = document.getElementById('advice-id');
+const img = document.getElementById('image')
 
+let data;
 
 const fetchAdvice = async () =>{
     const response = await fetch('https://api.adviceslip.com/advice')
-    const data = await response.json()
+     data = await response.json()
     
     console.log(data)
 
     adviceHeaderEl.innerHTML = `Advice #${data.slip.id}`;
     adviceTextEl.innerHTML = `"${data.slip.advice}"`
+    // img.style.rotate = `${data.slip.id}deg`
+    
+
 }
 
 fetchAdvice()
 
 diceBtnEl.addEventListener('click', function(){
     fetchAdvice()
+    img.style.rotate = `${data.slip.id}deg`
+    diceBtnEl.style.backgroundColor = `hsl(${data.slip.id} 100% 66% / 1)`
+
+
 })
+
+
+// diceBtnEl.addEventListener('click', fetchAdvice)
